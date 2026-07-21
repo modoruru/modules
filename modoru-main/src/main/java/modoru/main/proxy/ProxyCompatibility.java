02bb1a6b6b9e2c14697a953e3926a8bbc050b4a6
justlofe <lofikpc@gmail.com>
2026-07-22 06:55:06 +0700

    add config check before loading proxy compatibility

--- modoru-main/src/main/java/modoru/main/proxy/ProxyCompatibility.java
+++ modoru-main/src/main/java/modoru/main/proxy/ProxyCompatibility.java
@@ -1,6 +1,7 @@
 package modoru.main.proxy;
 
 import io.netty.buffer.Unpooled;
+import io.papermc.paper.configuration.GlobalConfiguration;
 import modoru.main.player.UsernameFormatter;
 import modoru.main.storage.StorageClient;
 import net.minecraft.network.FriendlyByteBuf;
@@ -9,6 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.messaging.Messenger;
 import su.hitori.api.Hitori;
+import su.hitori.api.logging.LoggerFactory;
 import su.hitori.ux.storage.DataContainer;
 
 import java.util.HashMap;
@@ -16,13 +18,17 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.logging.Logger;
 
 public final class ProxyCompatibility {
 
+    private static final Logger LOGGER = LoggerFactory.instance().create();
     private static final String IDENTIFIER = "modoru:formatted_names";
 
     private final AtomicReference<StorageClient> storageReference;
     private final ExecutorService executorService;
+
+    private boolean loaded;
 
     public ProxyCompatibility(AtomicReference<StorageClient> storageReference, ExecutorService executorService) {
         this.storageReference = storageReference;
@@ -30,8 +36,16 @@ public final class ProxyCompatibility {
     }
 
     public void load() {
+        if(loaded) return;
         Messenger messenger = Bukkit.getMessenger();
         Plugin plugin = Hitori.instance().plugin();
+
+        if(!GlobalConfiguration.get().proxies.velocity.enabled) {
+            LOGGER.warning("Velocity proxy is not enabled in Paper's paper-global.yml config, compatibility would not be loaded.");
+            return;
+        }
+
+        loaded = true;
 
         messenger.registerIncomingPluginChannel(plugin, IDENTIFIER, this::acceptMessage);
         messenger.registerOutgoingPluginChannel(plugin, IDENTIFIER);
@@ -71,6 +85,9 @@ public final class ProxyCompatibility {
     }
 
     public void unload() {
+        if(!loaded) return;
+        loaded = false;
+
         Messenger messenger = Bukkit.getMessenger();
         Plugin plugin = Hitori.instance().plugin();
 
